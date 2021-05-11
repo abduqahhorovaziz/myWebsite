4fb75627f24a2160a8b85c56f594cdb58a740420
Azizbek <muradovich.a22@gmail.com>
2021-05-11 20:15:14 +0500

    Bu mening shaxsiy website qoralamam

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.animate', {
+    strings: [
+        'Dasturlash',
+        'Marketing',
+        'Dizayn',
+        'Biznes'
+    ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true
+});
